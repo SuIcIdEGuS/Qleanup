d28029b90c1df94d2ce20e9b4411bf522d593a3b
SuIcIdEGuS <97912955+SuIcIdEGuS@users.noreply.github.com>
2022-11-08 04:15:27 +0300

    Add files via upload

---
+++ Новая папка (2)/Qleanup2/Qleanup2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qleanup2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Form2 newForm2 = new Form2();
+            newForm2.Show();
+            this.Visible = false;
+            
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            Form2 newForm2 = new Form2();
+            newForm2.Show();
+            this.Visible = false;
+        }
+    }
+}
